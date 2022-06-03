abc281d094a11485d87e3e4e29bf47e5f74ba5f9
sashtje <sashtje.lefevre@gmail.com>
2022-06-03 18:50:17 +0300

    feat: add react-split

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+import Split from "react-split";
+
+function App() {
+  return (
+    <Split style={{ display: "flex" }}>
+      <div style={{ backgroundColor: "gray" }}>Pane1</div>
+      <div style={{ backgroundColor: "gray" }}>Pane2</div>
+    </Split>
+  );
+}
+
+export default App;
