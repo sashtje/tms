c5268025bbd288940690801cd65f52a070d81540
sashtje <sashtje.lefevre@gmail.com>
2022-06-04 22:02:05 +0300

    feat: add selection of the selected table row

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Split from "react-split";
 import { Table } from "antd";
 
@@ -8,6 +9,22 @@ import { columns, data } from "../model/data";
 import "./styles.scss";
 
 function App() {
+  const [selectedRow, setSelectedRow] = useState();
+
+  const onClickRow = ({ number }) => {
+    return {
+      onClick: () => {
+        setSelectedRow(number);
+      },
+    };
+  };
+
+  const setRowClassName = ({ number }) => {
+    return number === selectedRow
+      ? "table__selected-row table__row"
+      : "table__row";
+  };
+
   return (
     <Split minSize={0} className="page">
       <Table
@@ -18,6 +35,8 @@ function App() {
           pageSize: 10,
         }}
         className="table"
+        onRow={onClickRow}
+        rowClassName={setRowClassName}
       />
 
       <Map />
